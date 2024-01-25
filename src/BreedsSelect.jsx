9c4da48ebd58b5c670db956d6bc5e4eb95277e26
yusaku-nakada-satt <y_nakada@satt.jp>
2024-01-25 17:20:44 +0900

    station11

--- src/BreedsSelect.jsx
+++ src/BreedsSelect.jsx
@@ -1,4 +1,6 @@
 // @ts-check
+import { useState } from 'react'
+import React from 'react'
 
 export const BreedsSelect = props => {
   let dogList = []
@@ -9,7 +11,40 @@ export const BreedsSelect = props => {
       </option>,
     )
   }
-  return <select title="犬種">{dogList}</select>
+
+  const [dogImage, setDogImage] = useState([])
+  const selectRef = React.useRef()
+
+  const rundomDogImage = selectRef => {
+    console.log(selectRef)
+    const url = `https://dog.ceo/api/breed/shiba/images/random/${selectRef}`
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.message)
+        setDogImage(data.message)
+      })
+  }
+
+  let dogImageList = []
+  for (let i = 0; i < dogImage.length; i++) {
+    if (i >= 12) {
+      break
+    }
+    dogImageList.push(<img key={i} src={dogImage[i]} alt="犬" />)
+  }
+
+  return (
+    <div>
+      <select ref={selectRef} title="犬種">
+        {dogList}
+      </select>
+      <button onClick={() => rundomDogImage(selectRef.current.value)}>
+        表示
+      </button>
+      {dogImageList}
+    </div>
+  )
 }
 
 export default BreedsSelect
